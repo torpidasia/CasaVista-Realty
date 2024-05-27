5806b9b19c4db3a0beac0ed024010daafece1c80
torpidasia <missasiakhalid@gmail.com>
2024-05-27 23:12:29 +0500

    index js upadated

--- api/index.js
+++ api/index.js
@@ -3,26 +3,40 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
-import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import listingRouter from "./routes/listing.route.js";
+import path from "path";
 
 dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error: " + err);
+  });
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-mongoose
-  .connect(process.env.MONGO, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Error connecting to MongoDB: ", err));
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -33,5 +47,3 @@ app.use((err, req, res, next) => {
     message,
   });
 });
-
-export default app;
